ff4c9390e94ffb4c8fbf30fbaca6d296e10baeb6
Diogo Silva <dbtdsilva@gmail.com>
2017-06-13 23:00:06 +0100

    cloudstorage: httpd module already working (early version)

--- modules/access/cloudstorage/provider_httpd.h
+++ modules/access/cloudstorage/provider_httpd.h
@@ -1,44 +1,33 @@
 #include <string>
 
 #include "access.h"
+#include <vlc_httpd.h>
 
 using cloudstorage::IHttpd;
 
 class Httpd : public IHttpd {
 public:
-    Httpd( access_t *access ) :
-        p_access( access ), p_sys( (access_sys_t*) access->p_sys )
-    {}
-
+    Httpd( access_t *access );
     void startServer(uint16_t port, CallbackFunction request_callback,
-            void* data) override
-    {
-
-    }
-
-    void stopServer() override
-    {
-
-    }
+            void* data) override;
+    void stopServer() override;
 
     std::string getArgument(RequestData*,
-            const std::string& arg_name) override
-    {
-
-    }
-
+            const std::string& arg_name) override;
     std::string getHeader(RequestData*,
-            const std::string& header_name) override
-    {
-
-    }
-
-    int sendResponse(RequestData*, const std::string& response) override
-    {
-
-    }
-
+            const std::string& header_name) override;
+    int sendResponse(RequestData*, const std::string& response) override;
 private:
+    static int internalCallback( httpd_callback_sys_t * args, httpd_client_t *,
+            httpd_message_t * answer, const httpd_message_t * query_t );
     access_t *p_access;
     access_sys_t *p_sys;
+    httpd_host_t *host;
+    httpd_url_t *url_root, *url_login;
+
+    struct CallbackData {
+        IHttpd* obj;
+        CallbackFunction request_callback;
+        void* custom_data;
+    };
 };
