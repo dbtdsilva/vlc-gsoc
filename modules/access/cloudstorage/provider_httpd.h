eede9f933160a7dd13fa186ff5f859ed58488fda
Diogo Silva <dbtdsilva@gmail.com>
2017-06-14 15:02:31 +0100

    cloudstorage: Ihttpd has now a simpler abstraction and removed useless prints

--- modules/access/cloudstorage/provider_httpd.h
+++ modules/access/cloudstorage/provider_httpd.h
@@ -11,12 +11,6 @@ public:
     void startServer(uint16_t port, CallbackFunction request_callback,
             void* data) override;
     void stopServer() override;
-
-    std::string getArgument(RequestData*,
-            const std::string& arg_name) override;
-    std::string getHeader(RequestData*,
-            const std::string& header_name) override;
-    int sendResponse(RequestData*, const std::string& response) override;
 private:
     static int internalCallback( httpd_callback_sys_t * args, httpd_client_t *,
             httpd_message_t * answer, const httpd_message_t * query_t );
@@ -26,7 +20,6 @@ private:
     httpd_url_t *url_root, *url_login;
 
     struct CallbackData {
-        IHttpd* obj;
         CallbackFunction request_callback;
         void* custom_data;
     };
