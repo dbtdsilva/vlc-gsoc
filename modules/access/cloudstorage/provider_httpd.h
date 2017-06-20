2e2bb63695c7cd7938748058d54459d0c2a14eef
Diogo Silva <dbtdsilva@gmail.com>
2017-06-20 15:02:43 +0100

    cloudstorage: updated headers

--- modules/access/cloudstorage/provider_httpd.h
+++ modules/access/cloudstorage/provider_httpd.h
@@ -1,6 +1,31 @@
-#include <string>
+/*****************************************************************************
+ * provider_httpd.h: Inherit class IHttpd from libcloudstorage
+ *****************************************************************************
+ * Copyright (C) 2017
+ *
+ * Authors: Diogo Silva <dbtdsilva@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef VLC_CLOUDSTORAGE_PROVIDER_HTTPD_H
+#define VLC_CLOUDSTORAGE_PROVIDER_HTTPD_H
 
 #include "access.h"
+
+#include <string>
 #include <vlc_httpd.h>
 
 using cloudstorage::IHttpd;
@@ -24,3 +49,5 @@ private:
         void* custom_data;
     };
 };
+
+#endif
