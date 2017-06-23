cc1bc1db0760838d8e34306c334b7e6ce880f854
Diogo Silva <dbtdsilva@gmail.com>
2017-06-23 12:04:01 +0100

    access: http: the resource created allows to specify the method associated with it

--- modules/access/http/live.h
+++ modules/access/http/live.h
@@ -30,7 +30,8 @@ struct block_t;
 
 struct vlc_http_resource *vlc_http_live_create(struct vlc_http_mgr *mgr,
                                                const char *uri, const char *ua,
-                                               const char *ref);
+                                               const char *ref,
+                                               const char *method);
 struct block_t *vlc_http_live_read(struct vlc_http_resource *);
 
 #define vlc_http_live_get_status vlc_http_res_get_status
