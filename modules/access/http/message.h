5316bd219592cb6121fc739aae9711253e091b28
Diogo Silva <dbtdsilva@gmail.com>
2017-06-26 20:57:44 +0100

    access: http: added support for a body on a request (needed for requests that are not GETs)

--- modules/access/http/message.h
+++ modules/access/http/message.h
@@ -83,6 +83,20 @@ VLC_API int vlc_http_msg_add_header(struct vlc_http_msg *, const char *name,
 int vlc_http_msg_add_agent(struct vlc_http_msg *, const char *);
 
 /**
+ * Sets the message body
+ *
+ * Sets the Body field.
+ */
+VLC_API int vlc_http_msg_add_body(struct vlc_http_msg *, void *, size_t);
+
+/**
+ * Gets the message body size
+ *
+ * Gets the Body size.
+ */
+VLC_API size_t vlc_http_msg_get_body_size(const struct vlc_http_msg *);
+
+/**
  * Gets the agent field.
  *
  * Gets the User-Agent or Server header field.
