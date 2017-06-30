e2bc8acc7c142355ced795f075d01c2135ccccc6
Diogo Silva <dbtdsilva@gmail.com>
2017-06-30 16:09:00 +0100

    http2: updated it to support bigger files by sending them in blocks

--- modules/access/http/h2conn.c
+++ modules/access/http/h2conn.c
@@ -439,7 +439,20 @@ static struct vlc_http_stream *vlc_h2_stream_open(struct vlc_http_conn *c,
     const uint8_t *body = vlc_http_msg_get_body(msg);
     size_t body_size = vlc_http_msg_get_body_size(msg);
 
-    vlc_h2_conn_queue(conn, vlc_h2_frame_data(s->id, body, body_size, true));
+    size_t body_streamed_size = 0;
+    const size_t block_size = 16384;
+
+    bool last_block = false;
+    while (!last_block) {
+        if (body_streamed_size + block_size >= body_size)
+            last_block = true;
+        size_t length = last_block ? body_size - body_streamed_size : block_size;
+
+        vlc_h2_conn_queue(conn, vlc_h2_frame_data(s->id,
+                body + body_streamed_size, length, last_block));
+
+        body_streamed_size += length;
+    }
 
     s->older = conn->streams;
     if (s->older != NULL)
